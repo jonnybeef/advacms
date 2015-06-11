a119c620f1aa2f2d965541487315b177d9162c80
vagrant <vagrant@precise32.(none)>
2015-06-11 07:48:21 +0000

    Established Access to Database

--- CMS/app/controllers/menu_controller.rb
+++ CMS/app/controllers/menu_controller.rb
@@ -1,5 +1,9 @@
 class MenuController < ApplicationController
   def index
   	base_url = "https://api.appery.io/rest/1/db/collections/SS_Menu/"
+
+  			# get request to retrieve data
+		@items = HTTParty.get(base_url,
+			:headers => {"X-Appery-Database-Id" => "556a91b7e4b0d4930d294754"}).parsed_response
   end
 end
