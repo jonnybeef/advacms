16c021cd45fb79fb28bdae7aa902c871cf4cfb34
vagrant <vagrant@precise32.(none)>
2015-06-17 06:16:17 +0000

    Added Delete Action

--- CMS/app/controllers/menu_controller.rb
+++ CMS/app/controllers/menu_controller.rb
@@ -6,4 +6,21 @@ class MenuController < ApplicationController
 		@items = HTTParty.get(base_url,
 			:headers => {"X-Appery-Database-Id" => "556a91b7e4b0d4930d294754"}).parsed_response
   end
+
+  def delete
+
+  	#grab item id that needs to be deleted
+  	id = params[:id]
+  	item = params[:item]
+	item_menu = params[:item_menu]
+	description = params[:description]
+
+  	base_url = "https://api.appery.io/rest/1/db/collections/SS_Menu/#{id}"
+
+  	#grab item information
+  	HTTParty.delete(base_url,:body => {"Item" => "#{item}", "Item_Menu" => "#{item_menu}", "Description" => "#{description}"}.to_json,
+			:headers => {"X-Appery-Database-Id" => "556a91b7e4b0d4930d294754","Content-Type" => "application/json"})
+  	redirect_to '/'
+
+  end
 end
