2a9cc35b011bbf4a118b348159d573eca74a82bc
vagrant <vagrant@precise32.(none)>
2015-06-28 05:17:50 +0000

    Refactored Delete Function

--- CMS/app/controllers/menu_controller.rb
+++ CMS/app/controllers/menu_controller.rb
@@ -12,13 +12,16 @@ class MenuController < ApplicationController
 
   	#grab item information that needs to be deleted
   	id = params[:id]
-  	item = params[:item]
-	item_menu = params[:item_menu]
-	description = params[:description]
+    
+  	# item = params[:item]
+	# item_menu = params[:item_menu]
+	# description = params[:description]
   	base_url = "https://api.appery.io/rest/1/db/collections/SS_Menu/#{id}"
-puts "THIS IS THE ID  OF THE ITEM BEING DELETED: #{id}"
+
+# puts "THIS IS THE ID  OF THE ITEM BEING DELETED: #{id}"
+
   	#grab item information
-  	HTTParty.delete(base_url,:body => {"id" => "#{id}","item" =>"#{item}"}.to_json,
+  	HTTParty.delete(base_url,:body => {"id" => "#{id}"}.to_json,
 			:headers => {"X-Appery-Database-Id" => "556a91b7e4b0d4930d294754","Content-Type" => "application/json"})
   	#redirect_to '/'
 
